781db38cb1be05e1f30d3446d147f54869987731
PiotrDrechsler <pdrech@gmail.com>
2022-12-21 20:11:33 +0100

    git fix
    removed props from App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+
 
 import { Statistics } from './Statistics/Statistics.jsx';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions.jsx';
