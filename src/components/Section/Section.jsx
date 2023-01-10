02784b0f05137aeb4dfed6539dd17d1ab80565ed
PiotrDrechsler <pdrech@gmail.com>
2023-01-10 17:09:26 +0100

    Fix
    Reunused files deleted

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,14 +1,14 @@
 import PropTypes from 'prop-types';
-import s from '../Section/Section.module.css'
+import s from '../Section/Section.module.css';
 
-export const Section = ({title, children}) => (
-    <section className={s.feedback}>
-        <h2 className={s.feedback__title}> {title}</h2>
-        {children}
-    </section> 
+export const Section = ({ title, children }) => (
+  <section className={s.feedback}>
+    <h2 className={s.feedback__title}> {title}</h2>
+    {children}
+  </section>
 );
 
 Section.propTypes = {
-    title: PropTypes.string.isRequired,
-    children: PropTypes.node.isRequired
-}
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
